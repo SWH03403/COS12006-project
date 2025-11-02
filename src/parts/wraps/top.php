ccd40a923c0c343dff0dfd090ab5740b149a61c0
SWH03403 <106224482@student.swin.edu.au>
2025-11-02 13:06:01 +0700

    refactor: fix absolute paths among assets

--- src/parts/wraps/top.php
+++ src/parts/wraps/top.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title><?= $opts['title'] ?? 'Untitled' ?></title>
+<head class="flex-y">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DoD - Home</title>
+	<link rel="icon" type="image/png" href="/static/images/favicon.png">
+	<link rel="stylesheet" href="/static/css/style.css">
 </head>
-<body>
-	<?php render('wraps/header') ?>
-	<main>
+<body class="flex-y">
+<?php render('wraps/header') ?>
+<main>
