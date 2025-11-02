45b292ca2f0247f4f27b0745fe7fec3986b64a32
SWH03403 <106224482@student.swin.edu.au>
2025-11-02 09:39:55 +0700

    feat: handle csrf token in session

--- src/core/session.php
+++ src/core/session.php
@@ -29,4 +29,21 @@ class Session {
 		self::init();
 		$_SESSION[$key] = $val;
 	}
+
+	public static function pop(string $key): mixed {
+		$val = self::get($key);
+		unset($_SESSION[$key]);
+		return $val;
+	}
+}
+
+class Csrf {
+	private const KEY = 'csrf_token';
+
+	public static function new() { Session::set(self::KEY, bin2hex(random_bytes(32))); }
+	public static function get(): ?string { Session::get(self::KEY); }
+	public static function check(string $token): bool {
+		if (is_null(self::get())) { return false; }
+		return Session::pop(self::KEY) === $token;
+	}
 }
