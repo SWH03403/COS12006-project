8a934108b238de00874d3020066038ebf46bc6f4
SWH03403 <106224482@student.swin.edu.au>
2025-11-03 21:33:24 +0700

    refactor: redefine dirs as class constants & recursive file lister

--- init.php
+++ init.php
@@ -1,33 +1,36 @@
 <?php
 declare(strict_types = 1);
 
-const MIGRATIONS_TABLE = '__migrations';
-const COMPONENTS_DIR = __DIR__ . '/parts';
-const PAGES_DIR = __DIR__ . '/routes';
-define('ASSETS_DIR', dirname(__DIR__) . '/assets');
-define('DATABASE_URL', dirname(__DIR__) . '/db.sqlite');
-define('MIGRATIONS_DIR', dirname(__DIR__) . '/migrations');
+const DATABASE_URL = __DIR__ . '/db.sqlite';
+class Dirs {
+	public const ASSETS = __DIR__ . '/assets';
+	public const COMPONENTS = __DIR__ . '/src/parts';
+	public const CORE = __DIR__ . '/src/core';
+	public const MIGRATIONS = __DIR__ . '/migrations';
+	public const PAGES = __DIR__ . '/src/routes';
 
-/*
- * Thanks, @user2226755!
- * Source: https://stackoverflow.com/questions/24783862/list-all-the-files-and-folders-in-a-directory-with-php-recursive-function
- */
-function list_recursive(string $dir = __DIR__ . "/core"): array {
-	$files = [];
-	foreach (scandir($dir) as $name) {
-		if ($name == '.' || $name == '..') { continue; }
-		$path = "$dir/$name";
-		if (is_dir($path)) {
-			$files = array_merge($files, list_recursive($path, $files));
-		} else {
-			if (!str_ends_with($name, '.php')) { continue; }
-			array_push($files, $path);
+	/*
+	 * Thanks, @user2226755!
+	 * Source: https://stackoverflow.com/questions/24783862/list-all-the-files-and-folders-in-a-directory-with-php-recursive-function
+	 */
+	private static function list(string $dir): array {
+		$files = [];
+		foreach (scandir($dir) as $name) {
+			if ($name == '.' || $name == '..') { continue; }
+			$path = "$dir/$name";
+			if (is_dir($path)) {
+				$files = array_merge($files, self::list($path, $files));
+			} else {
+				if (!str_ends_with($name, '.php')) { continue; }
+				array_push($files, $path);
+			}
 		}
+		return $files;
 	}
 
-	return $files;
+	public static function core_modules(): array { return self::list(self::CORE); }
 }
 
-foreach (list_recursive() as $module) { require $module; }
+foreach (Dirs::core_modules() as $module) { require $module; }
 
 Router::route();
