ce85e788d433ab2dd4773341c73197ce1b866c36
SWH03403 <106224482@student.swin.edu.au>
2025-11-11 10:47:55 +0700

    feat: links renderer

--- src/core/render.php
+++ src/core/render.php
@@ -5,6 +5,15 @@ function render(string $component, ?array $g_data = null) {
 	require Dirs::COMPONENTS . "/$component.php";
 }
 
+function render_links(array $routes) {
+	$cur = Request::path();
+	foreach ($routes as $url => $disp) {
+		$is_cur = (str_starts_with($url, '/') && substr($url, 1) === $cur)?
+			' class="current-page"' : '';
+		echo "<li><a href=\"$url\"$is_cur>$disp</a></li>";
+	}
+}
+
 function render_page(array|callable|string $content, array $g_data = []) {
 	global $data;
 	$data = $g_data;
