0984562d6ff0b5eed736587712f4fb3c1eb7be5f
SWH03403 <106224482@student.swin.edu.au>
2025-11-15 19:08:23 +0700

    refactor: use variadic function to store parameters

--- src/core/render.php
+++ src/core/render.php
@@ -6,19 +6,17 @@ function input_id(): string {
 	return 'input-auto-' . (++$_input_counter);
 }
 
-function render(string $component, ?array $data = null) {
+function render(string $component, ...$data) {
 	global $_g_data;
-	if (is_null($data)) { $data = $_g_data; }
+	if (empty($data)) { $data = $_g_data; }
+	extract($data, EXTR_PREFIX_ALL, 'arg_');
 	require Dirs::COMPONENTS . "/$component.php";
 }
 
-function render_page(array|callable|string $content, array $data = []) {
-	global $_g_data;
-	$_g_data = $data;
+function render_page(string $root, ...$data) {
+	global $_g_data; $_g_data = $data;
 
 	render('wraps/top');
-	if (is_string($content)) { echo "$content"; }
-	elseif (is_array($content)) { foreach ($content as $c) { render($c); }}
-	else { $content(); }
+	render('root');
 	render('wraps/bottom');
 }
