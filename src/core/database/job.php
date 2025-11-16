b0db53e2f0d0245f4d209e976aef1e2b5454ede4
SWH03403 <106224482@student.swin.edu.au>
2025-11-16 16:46:27 +0700

    feat: group jobs into sections by categories

--- src/core/database/job.php
+++ src/core/database/job.php
@@ -18,8 +18,13 @@ class JobCategory {
 	}
 }
 
+class JobRequirement {
+	public array $must = [];
+	public array $opts = [];
+}
+
 class JobSalary {
-	public function construct(public int $min, public int $max, public string $currency) {}
+	public function __construct(public int $min, public int $max, public string $currency) {}
 }
 
 class Job {
@@ -33,7 +38,54 @@ class Job {
 		public JobSalary $salary,
 		public string $description,
 		public Range $experience,
+		public JobRequirement $reqs,
 		public DateTimeImmutable $created,
 		public DateTimeImmutable $updated,
 	) {}
+
+	public static function all(): array {
+		$mapped_categories = [];
+		foreach (JobCategory::all() as $cat) { $mapped_categories[$cat->id] = $cat; }
+		$db = Database::get();
+		foreach ($db->query('SELECT * FROM job ORDER BY name') as $row) {
+			extract($row, EXTR_OVERWRITE); // WARN: Extract SQL row
+			$items[$id] = new self(
+				$id,
+				$mapped_categories[$category_id],
+				$company,
+				$superior,
+				$name,
+				$location,
+				new JobSalary($salary_min, $salary_max, $salary_currency),
+				$description,
+				new Range($exp_min, $exp_max),
+				new JobRequirement(),
+				new DateTimeImmutable($created, new DateTimeZone('UTC')),
+				new DateTimeImmutable($updated, new DateTimeZone('UTC')),
+			);
+			$row['requirements'] = $reqs[$row['id']] ?? [];
+			$cates[$row['category_id']]['entries'][] = $row;
+		}
+		foreach ($db->query('SELECT * FROM job_requirement') as $row) {
+			extract($row, EXTR_OVERWRITE); // WARN: Extract SQL row
+			if (str_starts_with($row['name'], 'opt-')) { $items[$id]->reqs->opts[] = $value; }
+			else { $items[$id]->reqs->must[$name] = $value; }
+		}
+		return $items;
+	}
+}
+
+class JobSection {
+	private function __construct(public JobCategory $category, public array $entries = []) {}
+
+	public static function all(): array {
+		$sections = [];
+		foreach (Job::all() as $item) {
+			if (!isset($sections[$item->category->id])) {
+				$sections[$item->category->id] = new self($item->category);
+			}
+			$sections[$item->category->id]->entries[] = $item;
+		}
+		return $sections;
+	}
 }
