b85f56e84910ccd727c6c4b1e718d5556472bd4d
SWH03403 <106224482@student.swin.edu.au>
2025-11-17 18:04:55 +0700

    refactor: unify boxlink render in user page

--- src/routes/user.php
+++ src/routes/user.php
@@ -6,25 +6,19 @@ $user = Session::user();
 render_page(function() use ($user) {
 	$account = $user->account();
 	$applicant = $user->applicant();
-	$row = fn($key, $val) => "<span>$key:</span><span>$val</span>";
 
 	$display = html_sanitize($account->display);
 	echo "<h1>Hi, $display</h1>";
 
-	echo <<<'TEXT'
-	<article id="account-container" class="box flex-y">
-		<div class="flex">
-			<h2>Account Info</h2>
-			<div class="fill"></div>
-			<a class="edit-btn flex-y" href="/user/edit">Edit</a>
-		</div>
-		<div id="account-info">
-	TEXT;
-	echo $row('Email', html_sanitize($account->email));
-	echo $row('Created', $account->created->format(DATETIME_FORMAT));
-	echo $row('Updated', $account->updated->format(DATETIME_FORMAT));
-	echo $row('Is manager', $account->is_manager? 'Yes' : 'No');
-	echo '</div></article>';
+	render('boxlink', function() use (&$account) {
+		$row = fn($key, $val) => "<span>$key:</span><span>$val</span>";
+		echo '<div id="account-info">';
+		echo $row('Email', html_sanitize($account->email));
+		echo $row('Created', $account->created->format(DATETIME_FORMAT));
+		echo $row('Updated', $account->updated->format(DATETIME_FORMAT));
+		echo $row('Is manager', $account->is_manager? 'Yes' : 'No');
+		echo '</div>';
+	}, 'Account Info', '/user/edit', 'Edit', id: 'account-container');
 	render('profile', $applicant);
 },
 	title: 'Profile',
